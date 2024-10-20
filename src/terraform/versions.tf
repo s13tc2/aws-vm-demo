38b02563969e5d271dd3a1d2bfd5205926414ba9
s13tc2 <kmk9957@outlook.com>
2024-10-20 19:28:48 -0400

    init commit

---
+++ src/terraform/versions.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+  }
+  // run it back turbo 2
+  backend "s3" {
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.primary_region
+}
+
